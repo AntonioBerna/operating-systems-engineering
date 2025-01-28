8e01a1fd33a0c5d4379075b12ba448ee5dc24648
AntonioBerna <antoniobernardini@live.com>
2025-01-28 01:31:59 +0100

    I have implemented memcheck for process and thread inside Makefile removing all .sh files

--- ping-pong/process/memcheck.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in {1..10}
-do
-    valgrind --leak-check=full --track-origins=yes ./a.out $i
-done
--- ping-pong/thread/memcheck.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in {1..10}
-do
-    valgrind --tool=helgrind --history-level=approx ./a.out $i
-done
